573d5b937d1b0110ccc3b273a4082dd46a2e033b
rivaldofez <rivaldofez@gmail.com>
2023-02-05 23:32:09 +0700

    remove storyboard and setup root controllers

---
+++ Pokepedia/Controller/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Pokepedia
+//
+//  Created by Rivaldo Fernandes on 05/02/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
