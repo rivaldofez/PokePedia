ed3834045ab3fef95e896907fc8d079850840ff3
rivaldofez <rivaldofez@gmail.com>
2023-02-06 21:23:29 +0700

    add pokedex collection view in home view controller

--- Pokepedia/Controller/HomeViewController.swift
+++ Pokepedia/Controller/HomeViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private var pokedexCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        view.addSubview(pokedexCollectionView)
+        
+        pokedexCollectionView.delegate = self
+        pokedexCollectionView.dataSource = self
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        pokedexCollectionView.frame = view.bounds
+    }
+}
 
 
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    
 }
