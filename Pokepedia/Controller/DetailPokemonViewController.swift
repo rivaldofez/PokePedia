8049d3a0cac3852fe3dd102cdf4b100a3c07f303
rivaldofez <rivaldofez@gmail.com>
2023-02-10 19:51:37 +0700

    add custom segmented control for detail information

--- Pokepedia/Controller/DetailPokemonViewController.swift
+++ Pokepedia/Controller/DetailPokemonViewController.swift
@@ -9,6 +9,45 @@ import UIKit
 
 class DetailPokemonViewController: UIViewController {
     
+    private enum SectionTabs: String {
+        case about = "About"
+        case stat = "Base Stat"
+        case moves = "Moves"
+        
+        var index: Int {
+            switch self {
+            case .about:
+                return 0
+            case .stat:
+                return 1
+            case .moves:
+                return 2
+            }
+        }
+        
+        static let allCases = [about, stat, moves]
+    }
+    
+    private var sectionTabButtons: [UIButton] = SectionTabs.allCases.map { sectionCase in
+        let button = UIButton(type: .system)
+        button.setTitle(sectionCase.rawValue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
+        button.tintColor = .label
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }
+    
+    private lazy var sectionStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: sectionTabButtons)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .equalSpacing
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        return stackView
+        
+    }()
+    
+    
     private var chipType: [UIStackView] = ["Grass", "Fire", "Flying"].map { title in
         let stackView = UIStackView()
         
@@ -51,6 +90,26 @@ class DetailPokemonViewController: UIViewController {
         return stackView
     }()
     
+    private var leadingAnchors: [NSLayoutConstraint] = []
+    private var trailingAnchors: [NSLayoutConstraint] = []
+    
+    private var selectedTab: Int = 0 {
+        didSet {
+            for i in 0 ..< sectionTabButtons.count {
+                UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) { [weak self] in
+                    self?.sectionStackView.arrangedSubviews[i].tintColor = i == self?.selectedTab ? .label : .secondaryLabel
+                    
+                    self?.leadingAnchors[i].isActive = i == self?.selectedTab ? true : false
+                    self?.trailingAnchors[i].isActive = i == self?.selectedTab ? true : false
+                    
+                    self?.view.layoutIfNeeded()
+                }
+            }
+            
+            print(selectedTab)
+        }
+    }
+    
     private let pokemonImageView: UIImageView = {
        let imageview = UIImageView()
         imageview.translatesAutoresizingMaskIntoConstraints = false
@@ -59,6 +118,13 @@ class DetailPokemonViewController: UIViewController {
         return imageview
     }()
     
+    private let indicator: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,8 +135,39 @@ class DetailPokemonViewController: UIViewController {
 
         view.addSubview(pokemonTypeStackView)
         view.addSubview(pokemonImageView)
+        view.addSubview(sectionStackView)
+        view.addSubview(indicator)
         
         configureConstraints()
+        configureStackButton()
+    }
+    
+    private func configureStackButton(){
+        for (i, button) in sectionStackView.arrangedSubviews.enumerated(){
+            guard let button = button as? UIButton else { return }
+            
+            if i == selectedTab {
+                button.tintColor = .label
+            } else {
+                button.tintColor = .secondaryLabel
+            }
+            
+            button.addTarget(self, action: #selector(didTapTab(_:)), for: .touchUpInside)
+        }
+    }
+    
+    @objc private func didTapTab(_ sender: UIButton){
+        guard let label = sender.titleLabel?.text else { return }
+        switch label {
+        case SectionTabs.about.rawValue:
+            selectedTab = 0
+        case SectionTabs.stat.rawValue:
+            selectedTab = 1
+        case SectionTabs.moves.rawValue:
+            selectedTab = 2
+        default:
+            selectedTab = 0
+        }
     }
     
     private func configureConstraints(){
@@ -86,8 +183,32 @@ class DetailPokemonViewController: UIViewController {
 
         ]
         
+        for i in 0 ..< sectionTabButtons.count {
+            let leadingAnchor = indicator.leadingAnchor.constraint(equalTo: sectionStackView.arrangedSubviews[i].leadingAnchor)
+            leadingAnchors.append(leadingAnchor)
+            
+            let trailingAnchor = indicator.trailingAnchor.constraint(equalTo: sectionStackView.arrangedSubviews[i].trailingAnchor)
+            trailingAnchors.append(trailingAnchor)
+        }
+        
+        let sectionStackViewConstraints = [
+            sectionStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            sectionStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            sectionStackView.topAnchor.constraint(equalTo: pokemonTypeStackView.bottomAnchor, constant: 5),
+            sectionStackView.heightAnchor.constraint(equalToConstant: 35)
+        ]
+        
+        let indicatorConstraints = [
+            leadingAnchors[0],
+            trailingAnchors[0],
+            indicator.topAnchor.constraint(equalTo: sectionStackView.arrangedSubviews[0].bottomAnchor),
+            indicator.heightAnchor.constraint(equalToConstant: 4)
+        ]
+        
         NSLayoutConstraint.activate(pokemonImageViewConstraints)
         NSLayoutConstraint.activate(pokemonTypeStackViewConstraints)
+        NSLayoutConstraint.activate(sectionStackViewConstraints)
+        NSLayoutConstraint.activate(indicatorConstraints)
     }
 
 
