e6c83bd3acc56964333c6d400176059fde516ce7
rivaldofez <rivaldofez@gmail.com>
2023-02-13 22:33:07 +0700

    implement presenter home and delegate to view controller

--- Pokepedia/Module/Presenter/HomePresenter.swift
+++ Pokepedia/Module/Presenter/HomePresenter.swift
@@ -8,11 +8,28 @@
 import Foundation
 
 protocol HomePresenterProtocol {
-
+    var router: HomeRouterProtocol? { get set}
+    var interactor: HomeUseCase? { get set }
+    var view: HomeViewProtocol? { get set }
     
-    
+    func didGetPokemonData(with result: Result<[Pokemon], Error>)
 }
 
 class HomePresenter: HomePresenterProtocol{
+    var router: HomeRouterProtocol?
+    
+    var interactor: HomeUseCase?
+    
+    var view: HomeViewProtocol?
+    
+    func didGetPokemonData(with result: Result<[Pokemon], Error>) {
+        switch result {
+        case .success(let pokemons):
+            view?.updatePokemon(with: pokemons)
+        case .failure(let error):
+            view?.updatePokemon(with: error.localizedDescription)
+        }
+    }
+    
     
 }
