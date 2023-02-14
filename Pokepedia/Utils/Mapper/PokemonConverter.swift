9c4b4a963cf65b20c72af6172731fc2216d0db2d
rivaldofez <rivaldofez@gmail.com>
2023-02-14 19:19:02 +0700

    add color set for elements

--- Pokepedia/Utils/Mapper/PokemonConverter.swift
+++ Pokepedia/Utils/Mapper/PokemonConverter.swift
@@ -34,41 +34,83 @@ final class PokemonConverter {
         guard let pokemonElement = PokemonElement(rawValue: type) else { return "unknown"}
         switch pokemonElement {
         case .bug:
-            return "bug"
+            return "icon_bug"
         case .dark:
-            return "dark"
+            return "icon_dark"
         case .dragon:
-            return "dragon"
+            return "icon_dragon"
         case .electric:
-            return "electric"
+            return "icon_electric"
         case .fairy:
-            return "fairy"
+            return "icon_fairy"
         case .fighting:
-            return "fighting"
+            return "icon_fighting"
         case .fire:
-            return "fire"
+            return "icon_fire"
         case .flying:
-            return "flying"
+            return "icon_flying"
         case .ghost:
-            return "ghost"
+            return "icon_ghost"
         case .grass:
-            return "grass"
+            return "icon_grass"
         case .ground:
-            return "ground"
+            return "icon_ground"
         case .ice:
-            return "ice"
+            return "icon_ice"
         case .normal:
-            return "normal"
+            return "icon_normal"
         case .poison:
-            return "poison"
+            return "icon_poison"
         case .psychic:
-            return "psychic"
+            return "icon_psychic"
         case .rock:
-            return "rock"
+            return "icon_rock"
         case .steel:
-            return "steel"
+            return "icon_steel"
         case .water:
-            return "water"
+            return "icon_water"
+        }
+    }
+    
+    static func typeStringToColorName(type: String) -> String {
+        guard let pokemonElement = PokemonElement(rawValue: type) else { return "unknown"}
+        switch pokemonElement {
+        case .bug:
+            return "color_bug"
+        case .dark:
+            return "color_dark"
+        case .dragon:
+            return "color_dragon"
+        case .electric:
+            return "color_electric"
+        case .fairy:
+            return "color_fairy"
+        case .fighting:
+            return "color_fighting"
+        case .fire:
+            return "color_fire"
+        case .flying:
+            return "color_flying"
+        case .ghost:
+            return "color_ghost"
+        case .grass:
+            return "color_grass"
+        case .ground:
+            return "color_ground"
+        case .ice:
+            return "color_ice"
+        case .normal:
+            return "color_normal"
+        case .poison:
+            return "color_poison"
+        case .psychic:
+            return "color_psychic"
+        case .rock:
+            return "color_rock"
+        case .steel:
+            return "color_steel"
+        case .water:
+            return "color_water"
         }
     }
 }
