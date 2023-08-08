e4f9f37a651a08feebdb242cb01326134235d790
rivaldofez <rivaldofez@gmail.com>
2023-08-08 14:39:01 +0700

    implement favorite pokemon router

--- Pokepedia/Module/Favorite/Router/FavoriteRouter.swift
+++ Pokepedia/Module/Favorite/Router/FavoriteRouter.swift
@@ -5,4 +5,48 @@
 //  Created by Rivaldo Fernandes on 07/08/23.
 //
 
-import Foundation
+import UIKit
+
+
+protocol FavoriteRouterProtocol {
+    var entry: FavoriteViewController? { get set }
+    
+    static func start() -> FavoriteRouterProtocol
+    
+    func gotoDetailPokemon(with pokemon: Pokemon )
+}
+
+class FavoriteRouter: FavoriteRouterProtocol {
+    var entry: FavoriteViewController?
+    
+
+    static func start() -> FavoriteRouterProtocol {
+        let router = FavoriteRouter()
+        
+        //assign vip
+        var view: FavoriteViewProtocol = FavoriteViewController()
+        
+        var presenter: FavoritePresenterProtocol = FavoritePresenter()
+        
+        let interactor: FavoriteUseCase = Injection.init().provideFavorite()
+        
+        view.presenter = presenter
+        presenter.router = router
+        presenter.view = view
+        presenter.interactor = interactor
+        
+        router.entry = view as? FavoriteViewController
+        
+        return router
+    }
+    
+    func gotoDetailPokemon(with pokemon: Pokemon) {
+        let detailPokemonRouter = DetailPokemonRouter.createDetailPokemon(with: pokemon)
+        guard let detailPokemonView = detailPokemonRouter.entry else { return }
+        guard let viewController = self.entry else { return }
+        
+        viewController.hidesBottomBarWhenPushed = true
+        viewController.navigationController?.pushViewController(detailPokemonView, animated: true)
+        viewController.hidesBottomBarWhenPushed = false
+    }
+}
