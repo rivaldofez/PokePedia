a17f37f9c9d4259f4029e74385ad4481c9cc6aac
rivaldofez <rivaldofez@gmail.com>
2023-08-08 14:39:48 +0700

    implement favorite presenter

--- Pokepedia/Module/Favorite/Presenter/FavoritePresenter.swift
+++ Pokepedia/Module/Favorite/Presenter/FavoritePresenter.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol FavoritePresenterProtocol {
+    var router: FavoriteRouterProtocol? { get set }
+    var interactor: FavoriteUseCase? { get set }
+    var view: FavoriteViewProtocol? { get set }
+    
+    var isLoadingData: Bool { get set }
+    func didSelectPokemonItem(with pokemon: Pokemon)
+    
+}
+
+class FavoritePresenter: FavoritePresenterProtocol {
+    private let disposeBag = DisposeBag()
+    
+    var router: FavoriteRouterProtocol?
+    
+    var interactor: FavoriteUseCase?
+    
+    var view: FavoriteViewProtocol?
+    
+    var isLoadingData: Bool = false {
+        didSet {
+            view?.isLoadingData(with: isLoadingData)
+        }
+    }
+    
+    func didSelectPokemonItem(with pokemon: Pokemon) {
+        router?.gotoDetailPokemon(with: pokemon)
+    }
+}
+
