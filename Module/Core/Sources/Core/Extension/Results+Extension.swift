16d7d04754e373d0bf1910001267ed46987ace29
rivaldofez <rivaldofez@gmail.com>
2023-08-17 11:24:55 +0700

    add extension component to core module

---
+++ Module/Core/Sources/Core/Extension/Results+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldo Fernandes on 17/08/23.
+//
+
+import RealmSwift
+
+extension Results {
+
+  func toArray<T>(ofType: T.Type) -> [T] {
+    var array = [T]()
+    for index in 0 ..< count {
+      if let result = self[index] as? T {
+        array.append(result)
+      }
+    }
+    return array
+  }
+
+}
