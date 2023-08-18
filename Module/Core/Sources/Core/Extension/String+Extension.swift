1e781ee9f807ecc9ea11e031fdfb7972c3d0de86
rivaldofez <rivaldofez@gmail.com>
2023-08-18 23:28:55 +0700

    add module pokepedia favorite

---
+++ Module/Core/Sources/Core/Extension/String+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldo Fernandes on 17/08/23.
+//
+
+import Foundation
+
+extension String {
+    func utf8DecodedString() -> String {
+        let data = self.data(using: .utf8)
+        let message = String(data: data!, encoding: .nonLossyASCII) ?? ""
+        return message
+    }
+    
+    func utf8EncodedString() -> String {
+        let messageData = self.data(using: .nonLossyASCII)
+        let text = String(data: messageData!, encoding: .utf8) ?? ""
+        return text
+    }
+}
