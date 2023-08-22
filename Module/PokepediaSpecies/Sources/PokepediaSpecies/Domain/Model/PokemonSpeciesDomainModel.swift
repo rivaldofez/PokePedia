2133f348b820ecd6eb1211ef2f2b6066b728919d
rivaldofez <rivaldofez@gmail.com>
2023-08-23 06:47:52 +0700

    add domain model for pokemon species

---
+++ Module/PokepediaSpecies/Sources/PokepediaSpecies/Domain/Model/PokemonSpeciesDomainModel.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldo Fernandes on 23/08/23.
+//
+
+import Foundation
+
+public PokemonSpeciesDomainModel: Identifiable {
+    public let id: Int
+    public let baseHappines: Int
+    public let captureRate: Int
+    public let color: String
+    public let about: String
+    public let genderRate: String
+    public let genus: String
+    public let growthRate: String
+    public let habitat: String
+    public let hatchCounter: Int
+    public let isLegendary: Bool
+    public let isMythical: Bool
+    public let isBaby: Bool
+    public let shape: String
+    public let eggGroups: String
+}
+
