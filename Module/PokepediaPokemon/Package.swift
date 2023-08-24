efa68592bddf72e37f3af2291c2179c049ba4f12
rivaldofez <rivaldofez@gmail.com>
2023-08-24 09:12:47 +0700

    add pokemon entity in pokemon module

--- Module/PokepediaPokemon/Package.swift
+++ Module/PokepediaPokemon/Package.swift
@@ -14,13 +14,22 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/realm/realm-swift.git", branch: "master"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", branch: "master"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", branch: "main"),
+        .package(path: "../PokepediaCore")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PokepediaPokemon",
-            dependencies: []),
+            dependencies: [
+                .product(name: "RealmSwift", package: "realm-swift"),
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "RxSwift", package: "RxSwift"),
+                "PokepediaCore"
+            ]),
         .testTarget(
             name: "PokepediaPokemonTests",
             dependencies: ["PokepediaPokemon"]),
---
+++ Module/PokepediaPokemon/Sources/PokepediaPokemon/Data/Locale/Entity/PokemonEntity.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldo Fernandes on 24/08/23.
+//
+
+import RealmSwift
+
+class PokemonEntity: Object {
+    @Persisted(primaryKey: true) var id: Int
+    @Persisted var name: String
+    @Persisted var image: String
+    @Persisted var height: Float
+    @Persisted var weight: Float
+    @Persisted var baseExp: Int
+    @Persisted var baseStat: List<BaseStatEntity>
+    @Persisted var moves: String
+    @Persisted var type: String
+    @Persisted var abilities: String
+    @Persisted var isFavorite: Bool
+}
+
+class BaseStatEntity: Object {
+    @Persisted var name: String
+    @Persisted var effort: Int
+    @Persisted var value: Int
+    @Persisted var url: String
+}
