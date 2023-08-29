35348e30a35729b4a7e0ce6da9ecfe0bd535b3da
rivaldofez <rivaldofez@gmail.com>
2023-08-29 11:26:26 +0700

    disable testing files

--- Module/PokepediaSpecies/Tests/PokepediaSpeciesTests/PokepediaSpeciesTests.swift
+++ Module/PokepediaSpecies/Tests/PokepediaSpeciesTests/PokepediaSpeciesTests.swift
@@ -1,11 +1,4 @@
 import XCTest
+import RxSwift
 @testable import PokepediaSpecies
 
-final class PokepediaSpeciesTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PokepediaSpecies().text, "Hello, World!")
-    }
-}
---
+++ PokepediaApp/PokepediaTests/Domain/HomeInteractorTests.swift
@@ -0,0 +1,8 @@
+////
+////  HomeInteractor.swift
+////  PokepediaTests
+////
+////  Created by Rivaldo Fernandes on 29/08/23.
+////
+//
+//import Foundation
